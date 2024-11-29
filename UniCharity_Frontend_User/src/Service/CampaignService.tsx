963a558bcb3af851729590fc53c481eef7257987
DuyTranNhat <duytn1053@gmail.com>
2024-11-29 22:13:28 +0700

    jwt

--- UniCharity_Frontend_User/src/Service/CampaignService.tsx
+++ UniCharity_Frontend_User/src/Service/CampaignService.tsx
@@ -3,21 +3,6 @@ import { BASE_URL, CAMPAIGN_API } from "../Utils/Constant";
 import { CampaignFormRequest, CampaignGet, CampaignReponse, CampaignRequestRes, CampaignsReponse } from "../Models/Campaign";
 import { handleError } from "../Helper/ErrorHandler";
 
-const token = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkZXZ0ZXJpYS5jb20iLCJzdWIiOiJuZ3V5ZW52YW5hQGV4YW1wbGUuY29tIiwicm9sZSI6ImFkbWluIiwiaWQiOjEsImV4cCI6MTczMTczODUwMywiaWF0IjoxNzMxNzM0OTAzfQ.qVnMOjLqnqpQXFovHcL-W7vepC683A-5cSbVK1mpNyWuFMFzRXYrcrKuVoW069VXRe15JHG_R1ZE-1OGg3wiuQ"
-const dataPost = {
-    token: token
-}
-
-export const IntrospectAPI = async (page: number = 1, size: number = 4) => {
-    try {
-
-        const data = await axios.post<boolean>(`${BASE_URL}/auth/introspect`, dataPost);
-        return data;
-    } catch (error) {
-        handleError(error);
-    }
-};
-
 export const CampaignRequest = async (dataPost: CampaignFormRequest) => {
     try {
         const data = await axios.post<CampaignRequestRes>(`${BASE_URL}/campaigns/create-request-campaign`, dataPost);
@@ -42,7 +27,7 @@ export const CampaignGetByIDAPI = async (idCam: number = 4) => {
     try {
         const data = await axios.get<CampaignReponse>(`${CAMPAIGN_API}/get-by-id/${idCam}`, {
             headers: {
-                Authorization: `Bearer ${token}` // Thêm Bearer token vào header
+                Authorization: `Bearer ${`eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkZXZ0ZXJpYS5jb20iLCJzdWIiOiJuZ3V5ZW52YW5hQGV4YW1wbGUuY29tIiwicm9sZSI6ImFkbWluIiwiaWQiOjEsImV4cCI6MTczMjg5MzM1MSwiaWF0IjoxNzMyODg5NzUxfQ.BJT0vuiEhVT4w4SGDBNDfrK9HQkVe6_IDJxYW8Q3DJRnEzkStcTn7ewX5CC72ODTsfg0j7dp-h4_BXhwsX2RnQ`}`
             }
         });
         return data;
@@ -55,9 +40,13 @@ export const CampaignGetByStatusAPI = async (status: string, page: number = 1, s
     try {
         const data = await axios.get<CampaignsReponse>(`${CAMPAIGN_API}/get-by-status/${status}`, {
             params: { page, size },
-        });
+            headers: {
+                Authorization: `Bearer ${`eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkZXZ0ZXJpYS5jb20iLCJzdWIiOiJuZ3V5ZW52YW5hQGV4YW1wbGUuY29tIiwicm9sZSI6ImFkbWluIiwiaWQiOjEsImV4cCI6MTczMjg5MzM1MSwiaWF0IjoxNzMyODg5NzUxfQ.BJT0vuiEhVT4w4SGDBNDfrK9HQkVe6_IDJxYW8Q3DJRnEzkStcTn7ewX5CC72ODTsfg0j7dp-h4_BXhwsX2RnQ`}`
+            }
+        },);
         return data;
     } catch (error) {
         handleError(error);
     }
 };
+
